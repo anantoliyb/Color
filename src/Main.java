ff59b6b33dfa348b4d49c861a9feeeda07984bdf
anantoliyb <anantoliyb@softwareplanet.uk.com>
2015-05-21 11:28:58 +0300

    color

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("\33[33;1m\tyellow! \033[0m \t"); // yellow
+
+        System.out.println("\033[31;1m\tred text\033[0m \t"); //red
+
+        System.out.println("\033[32;1m\tgreen text\033[0m"); //green
+    }
+}
